d6d314504a458076018c8b601952512808dcb8c7
chenghao <chenghao@corp-ci.com>
2017-03-07 14:03:02 +0800

    test

--- template/src/main.js
+++ template/src/main.js
@@ -4,7 +4,7 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
